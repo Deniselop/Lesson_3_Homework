76b911700214d785263ae26cdbf659a6374a4fe7
Deniselop <volejkodenis302@gmail.com>
2022-11-24 20:00:31 +0300

    add commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int a = 9;
+        final int CONSTANT = 10;
+
+        if (a > 10){
+
+            System.out.println("a больше 10");
+
+            System.out.println("Конец метода");
+        }
+
+
+    }
+
+
+
+
+
+
+
+}
