1e35ff26401808326e2e59c92e918b1e761b1028
Deniselop <volejkodenis302@gmail.com>
2022-11-26 16:09:59 +0300

    add commit Task 3

---
+++ src/Homework/Group_1_Task_3.java
@@ -0,0 +1,45 @@
+package Homework;
+
+import java.util.Scanner;
+
+public class Group_1_Task_3 {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int a = in.nextInt();
+
+        if (a % 2 == 0){
+            System.out.println("Вы ввели чётное число!");
+        }else{
+            System.out.println("Вы ввели нечётное число!");
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
