3d2ee682474749d2189a25ed87503a8a16e219a2
Deniselop <volejkodenis302@gmail.com>
2022-11-26 16:27:27 +0300

    add commit Task 4

---
+++ src/Homework/Group_1_Task_4.java
@@ -0,0 +1,37 @@
+package Homework;
+
+import java.util.Scanner;
+
+public class Group_1_Task_4 {
+
+    public static void main(String[] args) {
+
+    Scanner in = new Scanner(System.in);
+
+    System.out.println("Введите температуру на улице: ");
+    String TEMPA  = "";
+    int t = in.nextInt();
+
+    if (t >= -5) TEMPA = "Тепло";
+    else
+    if (t <= -5) TEMPA = "Нормально";
+    else
+    if (t <= -20) TEMPA = "Холодно";
+
+        System.out.println(TEMPA);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
