f0abcbaa554962baa35f6727ca96a4c24361f37d
Deniselop <volejkodenis302@gmail.com>
2022-11-27 20:02:21 +0300

    add commit group_2_Task

---
+++ src/Homework/Group_2_Task_2.java
@@ -0,0 +1,16 @@
+package Homework;
+
+import java.util.Scanner;
+
+public class Group_2_Task_2 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int i;
+
+        for(i = 1; i < 100; i += 2){
+            System.out.println(i);
+        }
+
+    }
+}
