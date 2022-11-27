e801a018ef584cac9cd8ea8ccc95bc60ecd8a360
Deniselop <volejkodenis302@gmail.com>
2022-11-27 20:06:28 +0300

    add commit group_2_Task_2

--- src/Homework/Group_2_Task_2.java
+++ src/Homework/Group_2_Task_2.java
@@ -1,14 +1,11 @@
 package Homework;
 
-import java.util.Scanner;
-
 public class Group_2_Task_2 {
     public static void main(String[] args) {
 
-        Scanner in = new Scanner(System.in);
         int i;
 
-        for(i = 1; i < 100; i += 2){
+        for (i = 5; i >= 1; i--){
             System.out.println(i);
         }
 
