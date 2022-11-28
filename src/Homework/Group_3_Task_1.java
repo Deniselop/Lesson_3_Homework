9ed98d3f059a434248be817e532d6ae7e5404999
Deniselop <volejkodenis302@gmail.com>
2022-11-28 19:10:52 +0300

    add commit group_3_Task_1

---
+++ src/Homework/Group_3_Task_1.java
@@ -0,0 +1,28 @@
+package Homework;
+
+public class Group_3_Task_1 {
+    public static void main(String[] args) {
+
+        int c = 0;
+        int b = 1;
+        int a = 1;
+
+
+        for (int i = 0; i < 11; i++){
+
+            c = a + b;
+            a = b;
+            b = c;
+            System.out.println(c);
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
